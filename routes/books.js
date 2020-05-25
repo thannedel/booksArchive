37b2c3928b388451460df4ec2d3d5f913db0bafa
thannedel <57347092+thannedel@users.noreply.github.com>
2020-05-25 12:29:37 +0200

    search by writer, title, categories

--- routes/books.js
+++ routes/books.js
@@ -1,97 +1,114 @@
 const express = require("express");
 const router = express.Router();
-const db = require('../config/database');
-const Book = require('../models/Book');
-const Sequelize = require('sequelize');
+const db = require("../config/database");
+const Book = require("../models/Book");
+const Sequelize = require("sequelize");
 const Op = Sequelize.Op;
 
 // Get book list
 
-router.get('/', (req, res) =>
-    Book.findAll()
-    .then(books =>
-        res.render('books', {
-            books
-        })).catch((err) => console.log(err)));
+router.get("/", (req, res) =>
+  Book.findAll()
+  .then((books) =>
+    res.render("books", {
+      books,
+    })
+  )
+  .catch((err) => console.log(err))
+);
 //Display add book
-router.get('/add', (req, res) => res.render('add'));
+router.get("/add", (req, res) => res.render("add"));
 
 //Add a book
-router.post('/add', (req, res) => {
+router.post("/add", (req, res) => {
+  let {
+    title,
+    writer,
+    description,
+    categories
+  } = req.body;
+  let errors = [];
 
+  //Validate Fields
+  if (!title) {
+    errors.push({
+      text: "Please add a title",
+    });
+  }
+  if (!writer) {
+    errors.push({
+      text: "Please add the writer",
+    });
+  }
 
-    let {
+  //Check of error
+  if (errors.length > 0) {
+    res.render("add", {
+      errors,
+      title,
+      writer,
+      description,
+      categories,
+    });
+  } else {
+    if (!description) {
+      description = "Unknown";
+    } else {
+      description = `${description}`;
+    }
+
+    // Make lowercase and remove space after comma
+    categories = categories.toUpperCase().replace(/, /g, ",");
+
+    // Insert into table
+    Book.create({
         title,
         writer,
         description,
-        categories
-    } = req.body;
-    let errors = [];
-
-    //Validate Fields
-    if (!title) {
-        errors.push({
-            text: 'Please add a title'
-        });
-    }
-    if (!writer) {
-        errors.push({
-            text: 'Please add the writer'
-        });
-    }
-
-    //Check of error
-    if (errors.length > 0) {
-        res.render('add', {
-            errors,
-            title,
-            writer,
-            description,
-            categories
-        });
-    } else {
-
-        if (!description) {
-            description = 'Unknown';
-        } else {
-            description = `${description}`;
-        }
-
-        // Make lowercase and remove space after comma
-        categories = categories.toLowerCase().replace(/, /g, ',');
-        // Insert into table
-        Book.create({
-                title,
-                writer,
-                description,
-                categories
-            })
-            .then(book => res.redirect('/books'))
-            .catch(err => console.log(err));
-    }
-
+        categories,
+      })
+      .then((book) => res.redirect("/books"))
+      .catch((err) => console.log(err));
+  }
 });
 
 //Search for books
-router.get('/search', (req, res) => {
-    let {
-        term
-    } = req.query;
+router.get("/search", (req, res) => {
+  let {
+    term
+  } = req.query;
 
-    // Make lowercase
-    //term = term.toLowerCase();
+  // Make uppercase
+  term = term.toUpperCase();
 
-    Book.findAll({
-            where: {
-                writer: {
-                    [Op.like]: '%' + term + '%'
-                }
+
+  Book.findAll({
+      where: {
+        [Op.or]: [{
+            categories: {
+              [Op.like]: "%" + term + "%",
+            },
+          },
+          {
+            title: {
+              [Op.like]: "%" + term + "%",
             }
-        })
-        .then(books => res.render('books', {
-            books
-        }))
-        .catch(err => console.log(err));
+          },
+          {
+            writer: {
+              [Op.like]: "%" + term + "%",
+            }
+          },
+        ]
+      },
+
+    })
+    .then((books) =>
+      res.render("books", {
+        books,
+      })
+    )
+    .catch((err) => console.log(err));
 });
 
 module.exports = router;
